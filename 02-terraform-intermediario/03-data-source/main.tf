2cf6522cfffdf9e54d1b29600a4b151c014d9552
Raphael Oliveira <rapahaeru@gmail.com>
2025-06-16 08:33:14 -0300

    data-sources: levantando EC2 consumindo ami

---
+++ 02-terraform-intermediario/03-data-source/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+
+backend "s3" {
+    bucket  = "tf-state010438491289"
+    key     = "dev/03-data-source/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "cael-terraform"
+  }
+
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+
+  # Como já tenho meu profile criado, não é necessário utilizar as chaves de acesso abaixo
+  # access_key = ""
+  # secret_key = ""
+}
