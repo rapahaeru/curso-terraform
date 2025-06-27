da525192e91639d0b222931176b945fed162cc6d
Raphael Oliveira <rapahaeru@gmail.com>
2025-06-27 09:33:26 -0300

    modules: corrige policies e acl (funcionando acesso público aos arquivos)

--- 02-terraform-intermediario/05-modules/main.tf
+++ 02-terraform-intermediario/05-modules/main.tf
@@ -39,30 +39,10 @@ module "website" {
   source = "./s3_module"
 
   name  = random_pet.website.id
-  acl   = "public-read"
   files = "${path.root}/website"
 
   website = {
     index_document = "index.html"
     error_document = "error.html"
   }
-
-  policy = <<EOT
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${random_pet.website.id}/*"
-            ]
-        }
-    ]
-}
-EOT
 }
--- 02-terraform-intermediario/05-modules/s3_module/main.tf
+++ 02-terraform-intermediario/05-modules/s3_module/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.name
-  #   acl    = var.acl
-  #   policy = var.policy
+  # acl    = var.acl
+  # policy = var.policy
   tags = var.tags
 
   dynamic "website" {
@@ -15,6 +15,23 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.this.arn}/*"
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_ownership_controls" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -25,25 +42,19 @@ resource "aws_s3_bucket_ownership_controls" "this" {
 
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket                  = aws_s3_bucket.this.id
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
 }
-
-# resource "aws_s3_bucket_acl" "this" {
-#   bucket = aws_s3_bucket.this.id
-#   acl    = var.acl
-# }
 
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
-    status    = "Enabled"
+    status     = "Enabled"
     mfa_delete = "Disabled"
   }
 }
-
 
 module "objects" {
   source = "./s3_object"
